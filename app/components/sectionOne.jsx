2e71f36c8f193b1b99df32403ddcf08858b9d263
Henry Odunsi <henryodunsi05@gmail.com>
2025-03-10 13:07:13 +0100

    Update sectionOne component animations and modify layout metadata for branding

--- app/components/sectionOne.jsx
+++ app/components/sectionOne.jsx
@@ -19,15 +19,15 @@ const sectionOne = ({
   const pageVariants = {
     initial: {
       opacity: 0,
-      x: -100,
+      y: -100,
     },
     animate: {
       opacity: 1,
-      x: 0,
+      y: 0,
     },
     exit: {
       opacity: 0,
-      x: 100,
+      y: 100,
     },
   };
 
